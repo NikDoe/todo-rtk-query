40c56caa974032d4f64cb3c9080b24b3131224f6
NikDoe <nikd0e@yandex.ru>
2022-11-28 10:13:54 +0300

    feature(rtk): add CRUD for todos;
    - Add mutations to the slice;
    - Apply mutation hooks in TodoList;
    - Invalidate cache, and auto-refetch;
    - Transforming the response in descending order;

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,11 +3,43 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+	tagTypes: ['Todos'],
 	endpoints: build => ({
 		getTodos: build.query({
 			query: () => '/todos',
+			transformResponse: res => res.sort((a, b) => b.id - a.id),
+			providesTags: ['Todos'],
+		}),
+		addTodo: build.mutation({
+			query: todo => ({
+				url: '/todos',
+				method: 'POST',
+				body: todo,
+			}),
+			invalidatesTags: ['Todos'],
+		}),
+		updateTodo: build.mutation({
+			query: todo => ({
+				url: `/todos/${todo.id}`,
+				method: 'PATCH',
+				body: todo,
+			}),
+			invalidatesTags: ['Todos'],
+		}),
+		deleteTodo: build.mutation({
+			query: ({ id }) => ({
+				url: `/todos/${id}`,
+				method: 'DELETE',
+				body: id,
+			}),
+			invalidatesTags: ['Todos'],
 		}),
 	}),
 });
 
-export const { useGetTodosQuery } = apiSlice;
+export const {
+	useGetTodosQuery,
+	useAddTodoMutation,
+	useUpdateTodoMutation,
+	useDeleteTodoMutation,
+} = apiSlice;
